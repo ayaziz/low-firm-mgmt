914f5da2b3d30193cc120841510440d25d8a8d2d
Atef.Aziz <atef.aziz@softwareone.com>
2026-02-24 11:50:44 +0200

    Gaps Deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://api:4000'}/api/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://backend:4000'}/api/:path*`,
       },
     ];
   },
